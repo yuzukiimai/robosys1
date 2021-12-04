2bd810ad1e50efe1792d9daa936d3c1fa285c851
Yuzuki Imai <yuzuki200110044779@gmail.com>
2021-12-04 07:33:31 +0000

    second commit

--- myled.c
+++ myled.c
@@ -4,55 +4,104 @@
    #include <linux/device.h>
    #include <linux/uaccess.h>
    #include <linux/io.h>
+   #include <linux/delay.h> 
 
 
-MODULE_AUTHOR("Yuzuki Imai");
+MODULE_AUTHOR("Ryuichi Ueda and Yuzuki Imai");
 MODULE_DESCRIPTION("driver for LED control");
 MODULE_LICENSE("GPL");
-MODULE_VERSION("0.0.1");
+MODULE_VERSION("0.0.3");
 
 static dev_t dev;
 static struct cdev cdv;
 static struct class *cls = NULL;
 static volatile u32 *gpio_base = NULL;
+static int LED_gpio[4] = {25, 23, 24, 26};
 
 
 static ssize_t led_write(struct file* flip, const char* buf, size_t count, loff_t* pos)
 {
+        int i;
 	char c;
 	if(copy_from_user(&c, buf, sizeof(char)))
 		return -EFAULT;
 
-      // 	 printk(KERN_INFO "receive %c\n", c);
 
 	if(c == '0') {
 		gpio_base[10] = 1 << 25;
+                gpio_base[10] = 1 << 23;
+                gpio_base[10] = 1 << 24;
+                gpio_base[10] = 1 << 26;
+
 	}
 	else if(c == '1') {
+
 		gpio_base[7] = 1 << 25;
+                gpio_base[10] = 1 << 23;
+                gpio_base[10] = 1 << 24;
+                gpio_base[10] = 1 << 26;
+
+
+
 	}
+    
+        else if(c == '2') {
+                  gpio_base[7] = 1 << 23;   
+                  gpio_base[10] = 1 << 25;
+                  gpio_base[10] = 1 << 24;
+                  gpio_base[10] = 1 << 26;
+        }
+       
+        else if(c == '3') {
+                   gpio_base[7] = 1 << 24;
+                   gpio_base[10] = 1 << 23;
+                   gpio_base[10] = 1 << 25;
+                   gpio_base[10] = 1 << 26;
+          }
+       
+        else if(c == '4') {
+             
+                  gpio_base[10] = 1 << 23;
+                  gpio_base[10] = 1 << 24;
+                  gpio_base[10] = 1 << 25;
+                  gpio_base[7] = 1 << 26;
+                 ssleep(1);
+
+}
+      
+        else if(c == '5') {
+                      
+              for (i = 0; i < 4; i++) {
+                      ssleep(2);
+                      gpio_base[7] = 1 << LED_gpio[i]; 
+                      gpio_base[10] = 1 << LED_gpio[i-1];
+                      
+                     if(i==0) {
+                               gpio_base[7] = 1 << 26;
+
+                              }       
+               
+                     else if(i==3){
+
+                                gpio_base[7] = 1 << 25;
+
+                                  }
+       
+                     else  gpio_base[10] = 1 << 26;
+
+                }                  
+ }
+
 
 	return 1;
 }	
-static ssize_t sushi_read(struct file* flip, char* buf, size_t count, loff_t* pos)
-{
-	int size = 0;
-	char sushi[] = {'s', 'u', 's', 'h', 'i', 0x0A};
-	if(copy_to_user(buf+size, (const char *)sushi, sizeof(sushi))) {
-		printk(KERN_ERR "sushi: copy_to_user failed.\n");
-		return -EFAULT;	
-         }
-
-	size += sizeof(sushi);
-	return size;
-}
 
 
 
 static struct file_operations led_fops = {
 	.owner = THIS_MODULE,
 	.write = led_write,
-	.read = sushi_read	
+	
 };
 
 
@@ -83,11 +132,30 @@ static struct file_operations led_fops = {
 
 	gpio_base = ioremap_nocache(0x3f200000, 0xA0);
 
-	const u32 led = 25;
-	const u32 index = led/10;
-	const u32 shift = (led%10)*3;
-	const u32 mask = ~(0x7 << shift);
-	gpio_base[index] = (gpio_base[index] & mask) | (0x1 << shift);
+	const u32 led1 = 25;
+	const u32 index1 = led1/10;
+	const u32 shift1 = (led1%10)*3;
+	const u32 mask1 = ~(0x7 << shift1);
+	gpio_base[index1] = (gpio_base[index1] & mask1) | (0x1 << shift1);
+
+     
+         const u32 led2 = 23;
+         const u32 index2 = led2/10;
+         const u32 shift2 = (led2%10)*3;
+         const u32 mask2 = ~(0x7 << shift2);
+          gpio_base[index2] = (gpio_base[index2] & mask2) | (0x1 << shift2);
+ 
+           const u32 led3 = 24;
+           const u32 index3 = led3/10;
+           const u32 shift3 = (led3%10)*3;
+           const u32 mask3 = ~(0x7 << shift3);
+           gpio_base[index3] = (gpio_base[index3] & mask3) | (0x1 << shift3);
+
+           const u32 led4 = 26;
+           const u32 index4 = led4/10;
+           const u32 shift4 = (led4%10)*3;
+           const u32 mask4 = ~(0x7 << shift4);
+           gpio_base[index4] = (gpio_base[index4] & mask4) | (0x1 << shift4);
 
  	 return 0;
 }
